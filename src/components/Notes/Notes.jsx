54f156e07aef7a81549248367c5d04cea99e97be
vanshikachaudhary-mst <vanshika.chaudhary@metasquaretech.com>
2024-10-07 12:28:36 +0530

    Edit Completed

--- src/components/Notes/Notes.jsx
+++ src/components/Notes/Notes.jsx
@@ -1,21 +1,23 @@
 import React from "react";
 
-const Notes = ({element, notes, setNotes, toggleModal}) => {          //"element","Notes","setNotes" pass as a destruction
+const Notes = ({element, notes, setNotes, toggleModal, seteditId}) => {          //"element","Notes","setNotes" pass as a destruction
   console.log(element);
 
    const removeHandler = (id) => {
     const newNotes = notes.filter((elm) => elm.id !== id);
     setNotes(newNotes);
+    
   };
 
-  // const editHandler = (id) =>{
-  //   notes.filter((elm)=>{
-  //     if(elm.id===id){
-  //       document.getElementById("edittitle").value=elem.title;
-  //       document.getElementById("editdesc").value=elem.desc;
-  //     }
-  //   } )
-  // }
+  const editHandler = (id) =>{
+    seteditId(id);
+    notes.filter((elm)=>{
+      if(elm.id===id){
+        document.getElementById("edittitle").value=elm.title;        //already written in the edit modal
+        document.getElementById("editdesc").value=elm.desc;
+      }
+    } )
+  }
  
   return (
     <div>
@@ -27,13 +29,23 @@ const Notes = ({element, notes, setNotes, toggleModal}) => {          //"element
               <div className="card-body text-capitalize">
                 <h5 className="card-title">{element.title}</h5>
                 <p className="card-text">{element.desc}</p>
-                <button
+                {/* <button
                   type="button"
                   className="btn btn-primary"
                   onClick={toggleModal} // Open modal on click
                 >
                   Edit
+                </button> */}
+                 
+                  <button
+                  type="button"
+                  className="btn btn-primary"
+                  onClick ={() => {
+                    toggleModal();               
+                    editHandler(element.id);  }}>
+                  Edit
                 </button>
+  
                 <button className="btn btn-danger mx-2" onClick={()=>{
                   removeHandler(element.id)
                 }} >Remove</button>
